f2dabe9d7881121c792dda4f69b8e33f0005a976
unknown <william.wong@senecacollege.ca>
2015-08-13 11:51:08 -0400

    Updated the views

--- application/views/trivia/triviadisplay/triviasecond.php
+++ application/views/trivia/triviadisplay/triviasecond.php
@@ -6,7 +6,7 @@
 <div class="triviamainbox"><!--  trivia second -->
 	<div class="questionbox" ng-init="loadQuestion()">
 		<h2 ng-show="!questionActive" class="questionnumberdone">Congratulations! You have completed the trivia activity.</h2>
-		<button id="startTrivia" ng-show="!questionActive" ng-click="playAgain()">Play Again</button>
+		<button ng-show="!questionActive" id="startTrivia" ng-click="playAgain()">Play Again</button>
 		<h2 ng-show="questionActive" class="questionnumber">Question Number: {{questionNumber}}</h2>
 		<h3 ng-show="questionActive">{{question}}</h3>
 	</div>
@@ -20,10 +20,14 @@
 	<!-- End of User choices -->
 	<!-- Lifelines -->
 	<div ng-show="questionActive" class="lifelines">
-	
+		<img src="../../../../public/img/layout/lifeline5050.png" width="40" height="40" alt="lifeline5050" ng-click="chooseLifeLine5050()" class="iconClick"/><span class="lifelinefont"> x {{lifeLine5050}}</span>
+		<div class="lifelinespacer"></div>
+		<img src="../../../../public/img/layout/lifelineHint.png" width="40" height="40" alt="lifelineHint" ng-click="" class="iconClick"/><span class="lifelinefont"> x {{lifeLineHint}}</span>
+		<div class="lifelinespacer"></div>
+		<img src="../../../../public/img/layout/lifelineAudience.png" width="40" height="40" alt="lifelineAudience" ng-click="" class="iconClick"/><span class="lifelinefont"> x {{lifeLineAudience}}</span>
 	</div>
 	<!-- End of Lifelines -->
-	<br /><br />
+	<br /><br /><br />
 	<div class="table">
 		<div class="score">
 			<span ng-show="questionActive">Score: {{score}} {{scoreType}}</span>
