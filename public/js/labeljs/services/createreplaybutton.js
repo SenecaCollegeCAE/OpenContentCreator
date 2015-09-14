a0811f4958766dc144ce995721b7f56f62478d69
unknown <william.wong@senecacollege.ca>
2015-09-14 13:25:20 -0400

    Fix some text

--- public/js/labeljs/services/createreplaybutton.js
+++ public/js/labeljs/services/createreplaybutton.js
@@ -1,7 +1,7 @@
 app.factory("createReplayButtonFactory", function($compile, $window) {	
 	return {
 		createButton: function(scope) {
-			var newElement = $('<button id="startAgain" ng-click="reloadActivity()">Play Again</button>');
+			var newElement = $('<button id="startAgain" ng-click="reloadActivity()">Play Again?</button>');
 			newElement.insertAfter('#statusText');
 			
 			$compile(newElement)(scope); //This adds the button element ng-click by dynamically binding after activity is completed
