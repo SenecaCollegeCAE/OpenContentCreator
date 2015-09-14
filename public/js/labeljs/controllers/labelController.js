ee4d818fbc206d00d55c6680cf54419b191e237f
unknown <william.wong@senecacollege.ca>
2015-09-14 13:23:05 -0400

    Added the scope for the play again

--- public/js/labeljs/controllers/labelController.js
+++ public/js/labeljs/controllers/labelController.js
@@ -1,3 +1,11 @@
-app.controller('labelController', function($scope, $rootScope, $location) {
+app.controller('labelController', function($scope, $rootScope, $location, $window, labelDraggingFactory) {
 	$scope.animating = true;
+	
+	$scope.init = function() {		
+		labelDraggingFactory.loadLabelDragging($scope);
+	};
+	
+	$scope.reloadActivity = function() {
+		$window.location.reload(true);
+	};
 });
