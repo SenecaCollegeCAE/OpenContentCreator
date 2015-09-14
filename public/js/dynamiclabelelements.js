ee0791d6f2b8c484aa03010f495ce80cfe8bd29a
unknown <william.wong@senecacollege.ca>
2015-09-14 13:21:34 -0400

    Changes to dynamic label elements

--- public/js/dynamiclabelelements.js
+++ public/js/dynamiclabelelements.js
@@ -14,11 +14,99 @@ $(document).ready(function() {
 	var canvas = $('#myCanvas')[0];
 	var context = canvas.getContext('2d');
 	
+	//*************************************** RUNS AFTER A POSTBACK /SUBMITTED (default running, not inside a function) *************************************
+	//USED FOR, if any form errors during the submit, the image and the labels will still be there
+	if($('#labelImageTarget').val() != "") {
+		var canvasCopy = $('<canvas></canvas>')[0]; //document.createElement('canvas')
+		var contextCopy = canvasCopy.getContext('2d');
+			
+		var flag = false;
+		var width = 650;
+		var height = 500;
+		var imageObj = new Image();
+	
+		imageObj.onload = function() {
+			
+			//Load the picture back into the canvas first
+			var ratio = 1;			
+			if(imageObj.width > width)
+				ratio = width / imageObj.width;
+			else if(imageObj.height > height)
+				ratio = height / imageObj.height;
+			
+			canvasCopy.width = imageObj.width;
+			canvasCopy.height = imageObj.height;
+			contextCopy.drawImage(imageObj, 0, 0);
+			
+			w = Math.floor(imageObj.width * ratio);
+			h = Math.floor(imageObj.height * ratio);
+			
+			if(w >= 630)
+				w = 610;
+		    if(h >= 480)
+		    	h = 460;
+
+		    context.drawImage(canvasCopy, 20, 20, w, h);
+		    canvasPush(); //the original canvas with the picture and no marker is clicked yet
+		    
+		    //Load the click locations back second
+		    var refreshedNumOfClicks = $('#labelNumOfTimesCreateWasClicked').val();
+		    var rightNowCoordsArray = JSON.parse($('#labelCoordsArray').val());
+		    
+		    for(var i = 0; i < rightNowCoordsArray.length; i++) {
+		    	points.push(rightNowCoordsArray[i]);
+		    }
+		    
+		    for(var i = 0; i < (refreshedNumOfClicks + 1); i++) {
+		    	
+		    	if(i <= refreshedNumOfClicks && points.length != 0) {
+		    		context.fillStyle = "#000000";
+					context.fillRect(points[i][0], points[i][1], 20, 20);
+					
+					context.strokeStyle = "#FF0000";
+					context.lineWidth = 2;
+					context.strokeRect(points[i][0], points[i][1], 20, 20);
+					
+					//**Need to write a number within the "dot" where the user clicks
+					context.fillStyle = "#FFFFFF";
+					context.font = "bold 10pt Arial";
+					context.fillText((i + 1).toString(), parseInt(points[i][0]) + 6, parseInt(points[i][1]) + 15);
+					
+					canvasPush();
+					flag = true;
+		    	}
+		    }
+
+		    if(!flag) { canvasPush(); }
+		    
+		    //Load the label elements third
+		    
+		    canvasClicks = parseInt($('#labelCurrentLabel').val());
+		    curentLabel = parseInt(refreshedNumOfClicks) + 2;
+		    
+		    //if(!flag) { canvasPush(); }
+		    
+		    for(var i = 0; i < (points.length - 1); i++) {
+		    	$('#CreateLabel').click();
+		    }
+		};
+		imageObj.src = $('#labelImageTarget').val();
+	}
+	//*************************************** END of RUNS AFTER A POSTBACK /SUBMITTED (default running, not inside a function) *************************************	
+	
 	//******************************* Create Label Element ****************************************************************************************************
 	$('#CreateLabel').click(function(e) {
+		var value = [];
+		
+		//Runs during postback
+		if($('#labelImageTarget').val() != "") {
+			value = JSON.parse($('#labelLabelArray').val());
+			$('#labelLabel1').val(value[0]);
+		} //End of running during postback
+		
 		if(currentLabel < 10) { //Max of 9 labels
 			var pElement = $('<p></p>').attr({'class': 'labelNumber'}).html(currentLabel + '. '); //childElement[0]
-			var inputElement = $('<input>').attr({type: 'text', name: 'labelLabel' + currentLabel, id: 'labelLabel' + currentLabel, maxlength: '90', size: '25', placeholder: 'Label ' + currentLabel}); //childElement[1]
+			var inputElement = $('<input>').attr({type: 'text', name: 'labelLabel' + currentLabel, id: 'labelLabel' + currentLabel, maxlength: '90', size: '25', placeholder: 'Label ' + currentLabel, value: value[numOfTimesCreateWasClicked + 1]}); //childElement[1]			
 			var spaceElement = $('<div></div>').attr({'class': "labelSpacer"}); //childElement[2]
 			
 			$('#parentElement').append(pElement, inputElement, spaceElement);
@@ -83,6 +171,35 @@ $(document).ready(function() {
 		console.log("h", h);
 	});
 	
+	//******************************* Form Submit Element ****************************************************************************************************
+	$('#labelForm').submit(function(e) { //Store values entered in hidden fields, when submitted the $_POST hidden fields can be used to refill the javascript input textboxes
+		var coordsArray = [];
+		var labelArray = [];
+		
+		$('#labelPostback').val("1"); //it is a postback/edit when set to 1(true), default is 0(false)
+		$('#labelCurrentLabel').val(currentLabel);
+		$('#labelNumOfTimesCreateWasClicked').val(numOfTimesCreateWasClicked);
+		
+		for(var i = 1; i < parseInt(numOfTimesCreateWasClicked + 2); i++) {
+			var ext = i.toString();
+			labelArray.push($('#labelLabel' + ext).val());
+		}
+		
+		var jsonSerializedLabels = JSON.stringify(labelArray); 
+		$('#labelLabelArray').val(jsonSerializedLabels); //parse and save the label values as an array into the hidden input field
+		
+		for(var i = 0; i < points.length; i++) {
+			var tempArray = [];
+			tempArray.push(points[i][0]);
+			tempArray.push(points[i][1]);
+			coordsArray.push(tempArray);
+		}
+		
+		var jsonSerializedCoords = JSON.stringify(coordsArray);
+		$('#labelCoordsArray').val(jsonSerializedCoords); //parse and save the coord values as an array(2d array) into the hidden input field
+		
+	}); //******************************* END of Form Submit Element ****************************************************************************************************
+	
 	/* This works on the canvas area
 	 * Below are functions that work within the canvas
 	 * Eg: Clicking on the canvas, clearing the canvas, ...etc
@@ -143,10 +260,6 @@ $(document).ready(function() {
 			reader.readAsDataURL(input.files[0]);
 		}
 	} //function readImage(input)
-	
-	function storeSession() { //Store values entered in hidden fields, when submitted the $_POST hidden fields can be used to refill the javascript input textboxes
-		
-	} //function storeSession()
 	
 	function canvasPush() { //runs anytime the canvas make a change, it will store that canvas session
 		canvasSteps++;
