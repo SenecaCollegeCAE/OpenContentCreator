4be264677b23498508911450873cc5a3d53bddb9
unknown <william.wong@senecacollege.ca>
2015-10-13 14:39:55 -0400

    Adding helper for the search

---
+++ application/controllers/helpers/storedUserInfoForActivityPage.php
@@ -0,0 +1,11 @@
+<?php
+	//retrieve the login user object
+	require_once("../../../resources/models/User.php");
+	session_start();
+	
+	//checks if there is a User session and the cookie has not expired
+	if(isset($_SESSION['User']) && $_SESSION['User'] != null && !empty($_COOKIE['PHPSESSID']) && $_COOKIE['PHPSESSID'] != null) {
+		$userObj = unserialize($_SESSION['User']); //Session obj is a class object
+		$userInfoArray = $userObj->getUserInfoFromSession();
+	}
+?>
