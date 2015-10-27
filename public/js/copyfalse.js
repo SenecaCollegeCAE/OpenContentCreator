92da93fe3aae698f733c3d6879a459ba3566b5ea
unknown <william.wong@senecacollege.ca>
2015-10-27 12:30:46 -0400

    Add the popup on the main page

---
+++ public/js/copyfalse.js
@@ -0,0 +1,2 @@
+$('body').append('<div id="alert">Error copying activity to your directory.</div>');
+$('#alert').fadeOut(3500);
---
+++ public/js/copytrue.js
@@ -0,0 +1,2 @@
+$('body').append('<div id="alert">Activity was successfully copied to your directory.</div>');
+$('#alert').fadeOut(3500);
