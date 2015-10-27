22c9f1f269f0af4dfbee04172b088deb423b8ac8
unknown <william.wong@senecacollege.ca>
2015-10-27 15:19:49 -0400

    Fix the redirect of pages

---
+++ htaccess_redirect.php
@@ -0,0 +1,5 @@
+<?php    
+    //header('Location: http://'.$_SERVER["HTTP_HOST"].'/OpenContentCreator/index.php');
+    header('Location: http://'.$_SERVER["HTTP_HOST"].'/occ/index.php');
+    exit;
+?>
